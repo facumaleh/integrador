a233ebce5f744266a23946e45c1b3ccac8eacea9
facundo <fmaleh@udesa.edu.ar>
2019-05-16 14:02:38 -0300

    puse la api de lo mas puntuado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.themoviedb.org/3/movie/%7Bmovie_id%7D/images?language=en-US&api_key=97feb9420c9c2cc675ee55c92f4a520c",
+  "url": "https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=97feb9420c9c2cc675ee55c92f4a520c",
   "method": "GET",
   "headers": {},
   "data": "{}"
