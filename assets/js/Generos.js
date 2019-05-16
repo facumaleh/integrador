5e65dc28984716337f0e5ce42e99b589ac3c92af
facumaleh <49995313+facumaleh@users.noreply.github.com>
2019-05-16 11:35:22 -0300

    Add files via upload
    cree el proyecto

---
+++ assets/js/Generos.js
@@ -0,0 +1,12 @@
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=97feb9420c9c2cc675ee55c92f4a520c",
+  "method": "GET",
+  "headers": {},
+  "data": "{}"
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
