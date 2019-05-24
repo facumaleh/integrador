c9d4f12b15a208f655a62498a3c68237fd71224e
facundo <fmaleh@udesa.edu.ar>
2019-05-24 11:54:05 -0300

    terminamos el carousel

--- assets/js/carrousel.js
+++ assets/js/carrousel.js
@@ -25,19 +25,19 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-// ESTO SIRVE PARA QUE GIRE SOLO EL CARROUSEL
-// 
-// var slideIndex = 0;
-// showSlides();
-//
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 5000); // Change image every 2 seconds
-// }
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
