7425f48cccb2b32488d0295628fa2bb20aa13f03
facundo <fmaleh@udesa.edu.ar>
2019-05-28 13:58:27 -0300

    termine el search

--- assets/js/resultados.js
+++ assets/js/resultados.js
@@ -5,23 +5,34 @@ window.onload = function() {
   var resultados = queryString.get("resultados")
   var url =  "https://api.themoviedb.org/3/movie/%7Bmovie_id%7D/keywords?api_key=97feb9420c9c2cc675ee55c92f4a520c"+ resultados
 
-  fetch(url)
-    .then(function(respuesta) {
+  var ruta = "HTTPS://image.tmdb.org/t/p/original"
 
-      return respuesta.json()
+  fetch(settings.url)
+    .then(function(response) {
+      return response.json();
     })
-    .then(function(respuesta) {
-      console.log(respuesta);
-      console.log(respuesta.results);
-      console.log(respuesta.results[0]);
+    .then(function(response) {
+      console.log("Esto es lo que me devuele, la API de movies: "+response);
+      console.log(response);
+      console.log(response.results);
 
-      var arrayDeDivs = document.querySelectorAll("resultados")
-      for (var i = 0; i < arrayDeDivs.length; i++) {
+  // capturo los 3 divs, donde voy a insertar la data de los estrenos
+  var arrayDeDivs = document.querySelectorAll("div.estrenos")
+  console.log(arrayDeDivs);
+  // voy a recorrer los divs, de los estrenos y les voy a insertar la darta
+        for (var i = 0; i < arrayDeDivs.length; i++) {
+          console.log(response.results[i].title);
+          console.log(response.results[i].backdrop_path);
+          console.log(response.results[i].release_date);
+
           console.log(arrayDeDivs[i]);
-          arrayDeDivs[i].querySelector("img").src = " https://image.tmdb.org/t/p/original/"+ respuesta.results[i].backdrop_path
-          arrayDeDivs[i].querySelector('.text').innerText = respuesta.results[i].original_title
-          arrayDeDivs[i].querySelector('.descripcion').innerText = respuesta.results[i].overview
-      }
+          arrayDeDivs[i].querySelector("h2 a").innerText = response.results[i].title
+  console.log(arrayDeDivs[i].querySelector("a img"));
+          arrayDeDivs[i].querySelector("a img").setAttribute("src", ruta + response.results[i].backdrop_path)
+          arrayDeDivs[i].querySelector("p.overview").innerText = response.results[i].overview
+          arrayDeDivs[i].querySelector("p").innerText = response.results[i].release_date
+        }
+
 
 
 
