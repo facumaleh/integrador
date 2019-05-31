c3a9853452fd1dab48bc2dde21c3043d89056e92
facundo <fmaleh@udesa.edu.ar>
2019-05-31 11:08:17 -0300

    arregle los detalles.html

--- scripts/movies/detalle.js
+++ scripts/movies/detalle.js
@@ -18,7 +18,7 @@ function getMovie(){
 
 	let movieId = sessionStorage.getItem("movieId");
 
-	const movieInfo = axios.get("https://api.themoviedb.org/3/movie/"+movieId+'?api_key='+API_KEY+'&language=en-US');
+	const movieInfo = axios.get("https://api.themoviedb.org/3/movie/"+movieId+'?api_key='+API_KEY+'&language=es-ES');
 	const movieCast = axios.get("https://api.themoviedb.org/3/movie/"+movieId+'/credits?api_key='+API_KEY)
 	Promise.all([movieInfo, movieCast])
 		.then( ([movieInfoResponse, movieCastResponse]) =>{
@@ -41,7 +41,7 @@ function getMovie(){
 			<div class="info">
 				<h2>${movie.title}</h2>
 				<ul>
-					<li><strong>Cast:</strong> `;
+					<li><strong>Elenco:</strong> `;
 					for (let i = 0; i < cast.length; i++) {
 						if (i != cast.length - 1) {
 							output += `${cast[i].name}, `;
@@ -50,7 +50,7 @@ function getMovie(){
 						}
 					}
 					output += `</li>
-					<li><strong>Genres:</strong> `;
+					<li><strong>Genros:</strong> `;
 					for(let i = 0; i < genres.length; i++){
 						if ( i != genres.length -1){
 							output += `${genres[i].name}, `;
@@ -59,24 +59,19 @@ function getMovie(){
 						}
 					}
 					output += `</li>
-					<li><strong>Tagline:</strong> ${movie.tagline} </li>
-					<li><strong>Release Date:</strong> ${movie.release_date}</li>
-					<li><strong>Runtime:</strong> ${movie.runtime} (min)</li>
+					<li><strong>slogan:</strong> ${movie.tagline} </li>
+					<li><strong>estreno:</strong> ${movie.release_date}</li>
+					<li><strong>duracion:</strong> ${movie.runtime} (min)</li>
 					<li><strong>Rating:</strong> ${movie.vote_average} / 10 <span id="smallText">(${movie.vote_count} votes)</span></li>
-					<li><strong>Revenue:</strong> ${revenue}</li>
-					<li><strong>Status:</strong> ${movie.status}</li>
-					<li><strong>Production companies:</strong> ${movie.production_companies[0].name}</li>
+					<li><strong>recaudacion:</strong> ${revenue}</li>
+					<li><strong>estado:</strong> ${movie.status}</li>
+					<li><strong>Productora:</strong> ${movie.production_companies[0].name}</li>
 				</ul>
-				<div class="buttons">
-					<a href="https://www.imdb.com/title/${movie.imdb_id}" target="_blank"> IMDB Link </a>
-					<a id="addToWatchList" onclick="addToList('${movie.id}')"> Add to watchlist </a>
-					<a class="twitter-share-button twitter" onclick="tweet('${movie.title}')"></a>
-					<a onclick="goBack()"> Go back </a>
-				</div>
+
 			</div>
 		</div>
 		<div class="plot">
-			<h3>Plot</h3>
+			<h3>resumen</h3>
 			<p>${movie.overview}</p>
 		</div>`;
 
@@ -104,95 +99,92 @@ function getMovie(){
 			document.getElementById("rec_title").style.display = "none";
 		});
 
-		//Gets the trailer link from youtube.
-		axios.get("https://api.themoviedb.org/3/movie/"+movieId+'/videos?api_key='+API_KEY+'&language=en-US')
-			.then((response)=>{
-				//Targets the first item in the results Array, that hold the "key" parameter.
-				let trailer = response.data.results;
 
-				// RANDOM NUMBER FOR TRAILER OUTPUT (ON EVERY PAGE LOAD, A DIFFERENT TRAILER WILL SHOW).
-				let min = 0;
-				// -1 so it takes into account if theres only 1 item in the trailer length( at position 0).
-				let max = trailer.length - 1;
-				min = Math.ceil(min);
-				max = Math.floor(max);
-				let trailerNumber = Math.floor(Math.random() * (max-min +1)) + min;
 
-				let output = `
-					<div class="video">
-					<iframe width="620" height="400" src="https://www.youtube.com/embed/${trailer[trailerNumber].key}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-					</div>`;
-				// Display the trailer.
-				let video = document.getElementById("trailer");
-				video.innerHTML = output;
-			})
+fetch("https://api.themoviedb.org/3/movie/"+movieId+'/videos?api_key='+API_KEY+'&language=es-ES')
+.then(function(response) {
+	return response.json();
+})
+.then(function(response) {
+	console.log(response);
+  console.log(response.results);
+  let movie = response.results;
+	let trailer = response.results;
 
-			.catch ((err)=>{
-				// Error message.
-				let trailerOutput = document.getElementById("trailer");
-				document.getElementById("trailer_title").style.display = "none";
-				trailerOutput.innerHTML =
-				 `<div class="trailer_error">
-					<h3>We are sorry! </h3>
-					<br>
-					<p>No video available at this moment. Try reloading the page.</p>
-				 </div>`;
-			});
+	// RANDOM NUMBER FOR TRAILER OUTPUT (ON EVERY PAGE LOAD, A DIFFERENT TRAILER WILL SHOW).
+	let min = 0;
+	// -1 so it takes into account if theres only 1 item in the trailer length( at position 0).
+	let max = trailer.length - 1;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	let trailerNumber = Math.floor(Math.random() * (max-min +1)) + min;
 
-		//Gets similar movies to the current one.
-		axios.get("https://api.themoviedb.org/3/movie/"+movieId+'/recommendations?api_key='+API_KEY+'&language=en-US&page=1')
-			.then ((response)=>{
-				const movie = response.data.results;
-				//Set the movie length (output) to 4.
-				movie.length = 4;
-				let output = "";
-				for(let i = 0; i < movie.length; i++){
-					output += `
-					<div class="card">
-						<div class="overlay">
-						<div class="movie">
-							<h2>${movie[i].title}</h2>
-								<p id="p_rating"><strong>Rating:</strong> <span>${movie[i].vote_average} / 10  <i class="material-icons star">star_rate</i></span> </p>
-								<p><strong>Release date:</strong> <span>${movie[i].release_date} <i class="material-icons date">date_range</i> </span></p>
-								<a onclick="movieSelected('${movie[i].id}')" href="#">Details</a>
-						</div>
-						</div>
-						<div class="card_img">
-							<img src="http://image.tmdb.org/t/p/w400/${movie[i].poster_path}" onerror="this.onerror=null;this.src='../images/imageNotFound.png';">
-						</div>
-					</div>
-					`;
-				}
-				//Target "recommended" and output the similar movies into it.
-				let recommended = document.getElementById("recommended");
-				recommended.innerHTML = output;
-				// Hide the previous page button of the first page.
-				document.getElementById("prev").style.display = "none";
-			})
-			//If there is an error, it logs it in the console.
-			.catch ((err)=>{
-				let recommended = document.getElementById("recommended");
-				document.getElementById("rec_title").style.display = "none";
-				document.querySelector(".page").style.display = "none";
-				recommended.innerHTML =
-				 `<div class="recommendations_error">
-					<h3>We are sorry! </h3>
-					<br>
-					<p>No recommendations are available at this moment. Please check later.</p>
-				 </div>`;
-			})
+	let output = `
+		<div class="video">
+		<iframe width="620" height="400" src="https://www.youtube.com/embed/${trailer[trailerNumber].key}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		</div>`;
+	// Display the trailer.
+	let video = document.getElementById("trailer");
+	video.innerHTML = output;
+})
+
+fetch("https://api.themoviedb.org/3/movie/"+movieId+'/recommendations?api_key='+API_KEY+'&language=es-ES&page=1')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    console.log(response);
+		const movie = response.results;
+		//Set the movie length (output) to 4.
+		movie.length = 4;
+		let output = "";
+		for(let i = 0; i < movie.length; i++){
+			output += `
+			<div class="card">
+				<div class="overlay">
+				<div class="movie">
+					<h2>${movie[i].title}</h2>
+						<p id="p_rating"><strong>Rating:</strong> <span>${movie[i].vote_average} / 10  <i class="material-icons star">star_rate</i></span> </p>
+						<p><strong>Release date:</strong> <span>${movie[i].release_date} <i class="material-icons date">date_range</i> </span></p>
+						<a onclick="movieSelected('${movie[i].id}')" href="#">Details</a>
+				</div>
+				</div>
+				<div class="card_img">
+					<img src="http://image.tmdb.org/t/p/w400/${movie[i].poster_path}" onerror="this.onerror=null;this.src='../images/imageNotFound.png';">
+				</div>
+			</div>
+			`;
+		}
+		//Target "recommended" and output the similar movies into it.
+		let recommended = document.getElementById("recommended");
+		recommended.innerHTML = output;
+		// Hide the previous page button of the first page.
+		document.getElementById("prev").style.display = "none";
+	})
+	//If there is an error, it logs it in the console.
+	.catch ((err)=>{
+		let recommended = document.getElementById("recommended");
+		document.getElementById("rec_title").style.display = "none";
+		document.querySelector(".page").style.display = "none";
+		recommended.innerHTML =
+		 `<div class="recommendations_error">
+			<h3>Perdon! </h3>
+			<br>
+			<p>No hay recomendacones</p>
+		 </div>`;
+	})
 }
 
 //Go back button function.
 function goBack(){
-	window.close();
+window.close();
 }
 
 // Take user to details page.
 function movieSelected(id){
-	sessionStorage.setItem("movieId", id);
-	location.replace("movie-page.html");
-	return false;
+sessionStorage.setItem("movieId", id);
+location.replace("movie-page.html");
+return false;
 }
 
 //Page number.
@@ -201,89 +193,17 @@ let pageNum = 1;
 //Previous page for recommended.
 const prev = document.getElementById("prev");
 prev.addEventListener("click", ()=>{
-	pageNum--;
-	recommendedPage(pageNum);
+pageNum--;
+recommendedPage(pageNum);
 })
 
 //Next page for recommended.
 const next = document.getElementById("next");
 next.addEventListener("click", ()=>{
-	pageNum++;
-	recommendedPage(pageNum);
+pageNum++;
+recommendedPage(pageNum);
 })
 
-//Recommended page change.
-function recommendedPage(pageNum){
-	let movieId = sessionStorage.getItem("movieId");
-	axios.get("https://api.themoviedb.org/3/movie/"+movieId+'/recommendations?api_key='+API_KEY+'&language=en-US&page='+pageNum)
-	.then ((response)=>{
-				let movie = response.data.results;
-				movie.length = 4;
-				let output = "";
-				for(let i = 0; i < movie.length; i++) {
-					output += `<div class="card">
-					<div class="overlay">
-					<div class="movie">
-						<h2>${movie[i].title}</h2>
-						<p id="p_rating"><strong>Rating:</strong> <span>${movie[i].vote_average} / 10  <i class="material-icons star">star_rate</i></span> </p>
-						<p><strong>Release date:</strong> <span>${movie[i].release_date} <i class="material-icons date">date_range</i> </span></p>
-						<a onclick="movieSelected('${movie[i].id}')" href="#">Details</a>
-					 </div>
-					</div>
-					<div class="card_img">
-						<img src="http://image.tmdb.org/t/p/w300/${movie[i].poster_path}" onerror="this.onerror=null;this.src='../images/imageNotFound.png';">
-					</div>
-					</div>`;
-				}
-				let recommended = document.getElementById("recommended");
-				recommended.innerHTML = output;
-				let totalPages = response.data.total_pages;
 
-				if (pageNum >= 2) {
-					document.getElementById("prev").style.display = "flex";
-				}
 
-				if( pageNum === totalPages) {
-					document.getElementById("next").style.display = "none";
-				} else if (pageNum === 1){
-					document.getElementById("prev").style.display = "none";
-					document.getElementById("next").style.display = "flex";
-				}
-			})
-	//If there is an error, it logs it in the console.
-	.catch ((err)=>{
-		console.log(err);
-	})
-}
-
-//Add to watchlist.
-function addToList(id){
-	let storedId = JSON.parse(localStorage.getItem("movies")) || [];
-	if(storedId.indexOf(id) === -1){
-		storedId.push(id);
-		localStorage.setItem("movies", JSON.stringify(storedId));
-
-		//Notification that it will be added to Watchlist.
-        const added = document.getElementById("added");
-        added.innerHTML = "Added to Watchlist!";
-        added.classList.add("added");
-		setTimeout(() => {
-            added.classList.remove("added");
-        }, 1500);
-	} else {
-		//Notification that it has already been added to the watchlist.
-		const alreadyStored = document.getElementById("alreadyStored");
-        alreadyStored.innerHTML = "Already in Watchlist!";
-        alreadyStored.classList.add("alreadyStored");
-		setTimeout(() => {
-            alreadyStored.classList.remove("alreadyStored");
-        }, 1500);
-	}
-}
-
-// Share on Twitter.
-function tweet(title) {
-	var strWindowFeatures = "location=yes,height=255,width=520,scrollbars=yes,status=yes";
-	var URL = "https://twitter.com/intent/tweet?text=Going to watch "+ title +' . Find something to watch @ https://pecko95.github.io/WatchSomething/&hashtags=watchSomething, whatToWatch';
-	var win = window.open(URL, "_blank", strWindowFeatures);
-}
+;
