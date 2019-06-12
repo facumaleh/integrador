ea24547d1cfb51f062a9b58a85d6289e6c0ee5e1
facundo <fmaleh@udesa.edu.ar>
2019-06-12 17:22:00 -0300

    ver mas

--- scripts/movies/estrenos.js
+++ scripts/movies/estrenos.js
@@ -175,3 +175,71 @@ function favorite(id){
         }, 1500);
 	}
 }
+
+document.querySelector('#vermas').addEventListener('click',
+function() {
+		fetch("https://api.themoviedb.org/3/movie/now_playing?api_key="+API_KEY+'&language=es-ES&page=2&region=US')
+	  .then(function(response) {
+	    return response.json();
+	  })
+		.then(function(response) {
+			console.log(response);
+		  console.log(response.results);
+		  let movie = response.results;
+			let output = "";
+			for(let i = 0; i < movie.length; i++){
+				let id = response.results[i].id;
+				id = JSON.stringify(id);
+				let favoriteMovies = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+				if(favoriteMovies.indexOf(id) === -1){
+					output += `
+					<div class="peliculas">
+						<div class="overlay">
+						<div class="addBtn">
+						<span><i class="material-icons favorite" onclick="favorite('${movie[i].id}')">favorite</i></span></div>
+						<div class="movie">
+							<h2>${movie[i].title}</h2>
+								<p id="p_rating"><strong>Rating:</strong> <span>${movie[i].vote_average} / 10 </span> </p>
+								<p><strong>Release date:</strong> <span>${movie[i].release_date} <i class="material-icons date">date_range</i> </span></p>
+								<a onclick="movieSelected('${movie[i].id}')" href="#">Detalles</a>
+						</div>
+						</div>
+						<div class="peliculas_img">
+							<img src="http://image.tmdb.org/t/p/w400/${movie[i].poster_path}" onerror="this.onerror=null;this.src='../images/imageNotFound.png';">
+						</div>
+					</div>
+					`;
+				} else {
+					output += `
+					<div class="peliculas">
+					<div class="overlay">
+					<div class="addBtn">
+					<span><i class="material-icons favoriteMarked" onclick="favorite('${movie[i].id}')">favorite</i></span></div>
+					<div class="movie">
+						<h2>${movie[i].title}</h2>
+							<p id="p_rating"><strong>Rating:</strong> <span>${movie[i].vote_average} / 10 </span> </p>
+							<p><strong>Release date:</strong> <span>${movie[i].release_date} <i class="material-icons date">date_range</i> </span></p>
+							<a onclick="movieSelected('${movie[i].id}')" href="#">DEtalles</a>
+					</div>
+					</div>
+					<div class="peliculas_img">
+						<img src="http://image.tmdb.org/t/p/w400/${movie[i].poster_path}" onerror="this.onerror=null;this.src='../images/imageNotFound.png';">
+					</div>
+				</div>
+				`;
+				}
+			}
+			// Display movies.
+			let movieInfo = document.getElementById("movies");
+			movieInfo.innerHTML += output;
+
+
+		})
+		// If theres an error, logs the error in console.
+		.catch( (err) =>{
+			console.log(err);
+		})
+		document.getElementById("vermas").style.display="none"
+}
+
+)
